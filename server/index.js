09efcbf7fe014ff882978f67cee3803984b22c78
stereotip13 <stereotip13@gmail.com>
2021-11-01 01:24:38 +0500

    01.10.21 ebana

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
+require('dotenv').config();
 const express = require('express');
-
-const PORT = 5000;
+const sequelize = require('./db');
+const models = require('./models/models.js');
+const PORT = process.env.PORT || 4000;
 
 const app = express();
+app.use(cors());
+app.use(express.json);
 
-app.listen(PORT, () => console.log(`Server started on port ${5000}`));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
